7bb43943a1f9745ecc05c2d7d13d1d50e3509d09
sportshead <me@sportshead.dev>
2024-01-25 19:57:19 +0000

    fzf-native

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,18 @@
 local builtin = require("telescope.builtin")
 
+require("telescope").setup({
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        },
+    },
+})
+require("telescope").load_extension("fzf")
+
 -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#find-files-from-project-git-root-with-fallback
 local function get_git_root() return vim.fn.system("git rev-parse --show-toplevel"):gsub("%s+$", "") end
 local function find_files()
--- lua/sportshead/packer.lua
+++ lua/sportshead/packer.lua
@@ -10,8 +10,11 @@ return require("packer").startup(function(use)
     use({
         "nvim-telescope/telescope.nvim",
         tag = "0.1.5",
-        -- or                          , branch = '0.1.x',
         requires = { { "nvim-lua/plenary.nvim" } },
+    })
+    use({
+        "nvim-telescope/telescope-fzf-native.nvim",
+        run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
     })
 
     use({
