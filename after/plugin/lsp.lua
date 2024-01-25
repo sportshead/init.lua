17cd4b30701347aaeef242e292bee1c2119d1740
sportshead <me@sportshead.dev>
2024-01-25 19:21:01 +0000

    fix tab complete

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -95,7 +95,7 @@ cmp.setup({
             if cmp.visible() then
                 local entry = cmp.get_selected_entry()
                 if not entry then
-                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                    cmp.confirm({ select = true })
                 else
                     cmp.confirm()
                 end
