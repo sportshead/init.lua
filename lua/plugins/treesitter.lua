4899afba171e32b5e90989702ad683dc1239f08f
sportshead <me@sportshead.dev>
2024-02-15 22:26:23 +0000

    treesitter: add vue, css, html to ensure_installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,19 @@ return {
         config = function()
             require("nvim-treesitter.configs").setup({
                 -- A list of parser names, or "all" (the five listed parsers should always be installed)
-                ensure_installed = { "javascript", "typescript", "go", "c", "lua", "vim", "vimdoc", "query" },
+                ensure_installed = {
+                    "javascript",
+                    "typescript",
+                    "go",
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "vue",
+                    "css",
+                    "html",
+                },
 
                 -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
