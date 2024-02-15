118331414657d7acc69a9fbdfd97c719aca27247
sportshead <me@sportshead.dev>
2024-02-15 22:07:22 +0000

    fix codeium tab key

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -17,16 +17,16 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         keys = {
-            { "<leader>re", ":Refactor extract ", mode = "x" },
-            { "<leader>rf", ":Refactor extract_to_file ", mode = "x" },
+            { "<leader>re",  ":Refactor extract ",             mode = "x" },
+            { "<leader>rf",  ":Refactor extract_to_file ",     mode = "x" },
 
-            { "<leader>rv", ":Refactor extract_var ", mode = "x" },
+            { "<leader>rv",  ":Refactor extract_var ",         mode = "x" },
 
-            { "<leader>ri", ":Refactor inline_var", mode = { "n", "x" } },
+            { "<leader>ri",  ":Refactor inline_var",           mode = { "n", "x" } },
 
-            { "<leader>rI", ":Refactor inline_func" },
+            { "<leader>rI",  ":Refactor inline_func" },
 
-            { "<leader>rb", ":Refactor extract_block" },
+            { "<leader>rb",  ":Refactor extract_block" },
             { "<leader>rbf", ":Refactor extract_block_to_file" },
         },
         config = true,
@@ -34,6 +34,10 @@ return {
     {
         "Exafunction/codeium.vim",
         event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            vim.g.codeium_no_map_tab = 1
+            vim.g.codeium_tab_fallback = ""
+        end,
     },
     {
         "norcalli/nvim-colorizer.lua",
