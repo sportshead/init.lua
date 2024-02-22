5afe26be8837c23a8e2af8990bffb523f8932291
sportshead <me@sportshead.dev>
2024-02-23 01:53:24 +0800

    commenting plugins

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -7,7 +7,20 @@ return {
         } },
     },
     {
-        "tpope/vim-commentary",
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        opts = {
+            enable_autocmd = false,
+        },
+        lazy = true,
+    },
+    {
+        "numToStr/Comment.nvim",
+        dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+        config = function()
+            require("Comment").setup({
+                pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+            })
+        end,
         event = { "BufReadPost", "BufNewFile" },
     },
     {
@@ -72,5 +85,10 @@ return {
         keys = {
             { "gf", function() require("actions-preview").code_actions() end, mode = { "v", "n" } },
         },
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = { signs = false },
     },
 }
