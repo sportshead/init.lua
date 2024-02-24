83306a7061b871ee8a9930039ba69080a88aee56
sportshead <me@sportshead.dev>
2024-02-25 02:51:16 +0800

    add gx

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -30,16 +30,16 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         keys = {
-            { "<leader>re",  ":Refactor extract ",             mode = "x" },
-            { "<leader>rf",  ":Refactor extract_to_file ",     mode = "x" },
+            { "<leader>re", ":Refactor extract ", mode = "x" },
+            { "<leader>rf", ":Refactor extract_to_file ", mode = "x" },
 
-            { "<leader>rv",  ":Refactor extract_var ",         mode = "x" },
+            { "<leader>rv", ":Refactor extract_var ", mode = "x" },
 
-            { "<leader>ri",  ":Refactor inline_var",           mode = { "n", "x" } },
+            { "<leader>ri", ":Refactor inline_var", mode = { "n", "x" } },
 
-            { "<leader>rI",  ":Refactor inline_func" },
+            { "<leader>rI", ":Refactor inline_func" },
 
-            { "<leader>rb",  ":Refactor extract_block" },
+            { "<leader>rb", ":Refactor extract_block" },
             { "<leader>rbf", ":Refactor extract_block_to_file" },
         },
         config = true,
@@ -90,5 +90,31 @@ return {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         opts = { signs = false },
+    },
+    {
+        "chrishrb/gx.nvim",
+        dev = true,
+        keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+        cmd = { "Browse" },
+        init = function()
+            vim.g.netrw_nogx = 1 -- disable netrw gx
+        end,
+        dependencies = { "nvim-lua/plenary.nvim" },
+        submodules = false, -- not needed, submodules are required only for tests
+
+        -- you can specify also another config if you want
+        opts = {
+            open_browser_app = "open", -- specify your browser app; default for macOS is "open", Linux "xdg-open" and Windows "powershell.exe"
+            handlers = {
+                plugin = true, -- open plugin links in lua (e.g. packer, lazy, ..)
+                github = true, -- open github issues
+                brewfile = true, -- open Homebrew formulaes and casks
+                package_json = true, -- open dependencies from package.json
+                search = true, -- search the web/selection on the web if nothing else is found
+            },
+            handler_options = {
+                search_engine = "duckduckgo",
+            },
+        },
     },
 }
