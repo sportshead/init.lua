391dc57ddc1a298cd4b71b6b4c1478f982cbd86e
sportshead <me@sportshead.dev>
2024-03-05 19:32:46 +0000

    +mdx, move {markdown-preview, typescript-tools} -> languages.lua

---
+++ lua/plugins/languages.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "iamcco/markdown-preview.nvim",
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = {
+                "markdown",
+            }
+        end,
+        ft = { "markdown" },
+    },
+    {
+        "pmizio/typescript-tools.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+        opts = {},
+    },
+    {
+        "davidmh/mdx.nvim",
+        config = true,
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        ft = { "mdx" },
+    },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -223,9 +223,4 @@ return {
             })
         end,
     },
-    {
-        "pmizio/typescript-tools.nvim",
-        dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-        opts = {},
-    },
 }
