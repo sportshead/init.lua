d5da089521c79c92f88df41be9d3f303ccf86387
sportshead <me@sportshead.dev>
2024-08-13 00:19:35 +0800

    black

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -289,6 +289,7 @@ return {
                     null_ls.builtins.formatting.prettierd,
                     null_ls.builtins.formatting.shfmt,
                     null_ls.builtins.formatting.clang_format,
+                    null_ls.builtins.formatting.black,
                 },
             })
         end,
