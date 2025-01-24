7ec87a845965d807e52a34ae7111bce1c26317cc
sportshead <me@sportshead.dev>
2025-01-24 18:45:50 +0000

    update

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -3,7 +3,6 @@ return {
         "L3MON4D3/LuaSnip",
         event = "InsertEnter",
         lazy = true,
-        dependencies = { "rafamadriz/friendly-snippets" },
         config = function()
             require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snip/" })
             require("luasnip.loaders.from_vscode").lazy_load()
